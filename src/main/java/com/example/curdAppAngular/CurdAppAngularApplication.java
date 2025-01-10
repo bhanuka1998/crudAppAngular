3619befefbd794bd79a6bdffa9c5b8c34ecc40ba
bhvi1 <bhanuka.vithanage@ebuilder.com>
2025-01-10 16:49:36 +0530

    Initial commit for the vanila prject

---
+++ src/main/java/com/example/curdAppAngular/CurdAppAngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.curdAppAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdAppAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdAppAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/curdAppAngular/CurdAppAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.curdAppAngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdAppAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
